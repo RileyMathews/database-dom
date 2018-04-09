827fde01258edcab7e13b532bdef5eb8f76df968
Riley Mathews <rileymathews80@gmail.com>
2018-04-09 15:03:28 -0500

    initial commit

---
+++ database.js
@@ -0,0 +1,9 @@
+//load database from local storage
+const loadDatabase = function (localStorageKey) {
+    // Get the string version of the database
+    const databaseString = localStorage.getItem(localStorageKey)
+
+    // Use JSON.parse() to convert the string back into an object
+    return JSON.parse(databaseString)
+}
+loadDatabase("inventory")
